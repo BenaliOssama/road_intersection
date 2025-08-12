e9b12a1708440d4535a490fba32f726c9265557c
obenali <oussama.benali@ump.ac.ma>
2025-08-12 18:22:15 +0100

    use time variation to update the cars

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
-mod cars;
-mod roads;
-mod traffic_lights;
-mod intersection;
+// mod cars;
+// mod roads;
+// mod traffic_lights;
+// mod intersection;
 
-use intersection::Intersection;
+//use intersection::Intersection;
 
 use sdl2::{event::Event, keyboard::Keycode,
 init, pixels::Color, rect::Rect, rect::Point};
+use std::time::Instant;
 
 fn main() {
+
     let sdl_context = init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
@@ -25,11 +27,21 @@ fn main() {
     let mut event_pump = sdl_context.event_pump().unwrap();
 
 
-    let mut y_pos = 0; // initial vertical position
-    let mut frame_count = 0;
+    let mut y_pos = 0f32; // initial vertical position
+    let speed = 60.0;
+    let mut last_time = Instant::now();
+
 
     'running: loop {
+        let now = Instant::now();
+        let dt = now.duration_since(last_time).as_secs_f32();
+        last_time = now ; 
 
+        // Update y position: move down by 5 pixels each frame
+        y_pos += speed * dt ;
+        if y_pos > 600.0 {
+            y_pos = 0.0 ;
+        }
         // Clear screen with black
         canvas.set_draw_color(sdl2::pixels::Color::RGB(0, 0, 0));
         canvas.clear();
@@ -42,22 +54,14 @@ fn main() {
         canvas.draw_line(Point::new(375, 0), Point::new(375, 600)).unwrap();
         canvas.draw_line(Point::new(425, 0), Point::new(425, 600)).unwrap();
 
-        
+
         // Set draw color to yellow
         canvas.set_draw_color(Color::RGB(255, 255, 0));
 
         // Draw a 50x50 square at fixed x=375, moving y=y_pos
-        let square = Rect::new(375, y_pos, 50, 50);
+        let square = Rect::new(375, y_pos as i32, 50, 50);
         canvas.fill_rect(square).unwrap();
 
-        frame_count += 1 ; 
-        // Update y position: move down by 5 pixels each frame
-        if frame_count % 17 == 0 {
-            y_pos += 1 ; 
-            if y_pos > 600 {
-                    y_pos = 0;
-            }
-        }
 
         // SDL2 uses double buffering for smooth graphics.
         // swaps the hidden buffer with the visible one
